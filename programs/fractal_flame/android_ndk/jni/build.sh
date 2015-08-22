8e9aa647fd2ca80fb8c9a381c75e7d181725722f
vova <vovaprog@yandex.ru>
2015-08-22 20:42:42 +0300

    android build

--- programs/fractal_flame/android_ndk/jni/build.sh
+++ programs/fractal_flame/android_ndk/jni/build.sh
@@ -1,3 +1,9 @@
 
 ndk-build
 
+rm -r ../../../fractal_flame_android/app/src/main/jniLibs
+
+mkdir ../../../fractal_flame_android/app/src/main/jniLibs
+
+cp -r ../libs/* ../../../fractal_flame_android/app/src/main/jniLibs
+
