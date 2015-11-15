0b68fbe7b3cd79a56b979124fd50a83057bd5334
vova <vovaprog@yandex.ru>
2015-11-15 12:25:22 +0300

    flame ui

--- programs/fractal_flame/ImageUtils.cpp
+++ programs/fractal_flame/ImageUtils.cpp
@@ -1,32 +1,16 @@
+#ifndef NO_IMAGE_FUNCTIONS
+
 #include <string.h>
 
-#ifndef NO_IMAGE_FUNCTIONS
-#   include <FreeImage.h>
-#   include "filesystem_utils.h"
-#endif
+#include <FreeImage.h>
 
 #include "ImageUtils.h"
 
 using namespace std;
 
-#ifndef NO_IMAGE_FUNCTIONS
-
-void /*ImageUtils::*/saveImage(const char *fileName, const char *fileType, const unsigned int *data, int pictureWidth, int pictureHeight)
+void saveImage(const char *fileName, const char *fileType, const unsigned int *data, int pictureWidth, int pictureHeight)
 { 
-    //int pictureSize = pictureWidth * pictureHeight;
-    
-	/*if(!tempData || pictureSize != tempPictureSize)
-	{
-		tempData=unique_ptr<unsigned int[]>(new unsigned int[pictureSize]);
-		tempPictureSize =  pictureSize;
-	}
-	
-	for(int i=0;i<pictureSize;i++)
-	{
-		tempData[i]=(0xff000000 | ((data[i] & 0xff) << 16) | (data[i] & 0xff00) | ((data[i] & 0xff0000) >>16)); 
-	}*/
-	
-	FIBITMAP* Image = FreeImage_ConvertFromRawBits((BYTE*)/*tempData.get()*/ data, pictureWidth, pictureHeight, pictureWidth * sizeof(unsigned int), 32, 
+	FIBITMAP* Image = FreeImage_ConvertFromRawBits((BYTE*) data, pictureWidth, pictureHeight, pictureWidth * sizeof(unsigned int), 32, 
 		0xFF0000, 0x00FF00, 0x0000FF, false); 
 	
 	if(strcmp(fileType,"png")==0)
--- programs/fractal_flame/ImageUtils.h
+++ programs/fractal_flame/ImageUtils.h
@@ -1,14 +1,4 @@
 #pragma once
 
-#include <memory>
-
-/*class ImageUtils{
-public:    
-    void saveImage(const char *fileName, const char *fileType, const unsigned int *data, int pictureWidth, int pictureHeight);    
-//private:    
-    //std::unique_ptr<unsigned int[]> tempData;
-    //int tempPictureSize = 0;
-};*/
-
 void saveImage(const char *fileName, const char *fileType, const unsigned int *data, int pictureWidth, int pictureHeight);
 
