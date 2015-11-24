0feb0384bee2c97f77bc4431537c3ce1d45c3622
vova <vovaprog@yandex.ru>
2015-11-24 09:45:02 +0300

    add sky

--- programs/fractal_glsl/fractal_glsl.cpp
+++ programs/fractal_glsl/fractal_glsl.cpp
@@ -16,13 +16,24 @@ static MovingValue koefReD(0.000113,0.00000323,-0.80,-0.79,0.000113,-0.8);
 static MovingValue koefImD(0.000099,0.00000245,0.1535,0.158,0.000099,0.156);
 static MovingValue movingHighBound(0.005,0.00008,0.4,1.4,-0.003,1.5);
 
-static unsigned int shaderProgram;
+static unsigned int shaderProgram, shaderSky;
 
 static ColorMapData clrMapData;
 static unsigned int *colorMap;
 
+
+static ColorMapData clrMapDataSky;
+static unsigned int *colorMapSky;
+
+
+
+
 static ConstantFps constFps(35);
 
+
+extern "C" {
+void glUseProgramObjectARB(GLhandleARB);
+}
 
 static void initColorMap()
 {
@@ -46,7 +57,30 @@ static void initColorMap()
 	Points.push_back(p);
 	
 	clrMapData.InitColorMap(iterLimit, Points, 150);
-	colorMap = clrMapData.GetColorMap();    
+	colorMap = clrMapData.GetColorMap();  
+	
+	
+	
+	Points = vector<ColorMapPoint>();
+	
+	p.Index = 0;
+	p.SetColor(0x000000ff);	
+	Points.push_back(p);	
+
+	p.Index = 40;
+	p.SetColor(0x3333ff);	
+	Points.push_back(p);
+
+	p.Index = 80;
+	p.SetColor(0x9999ff);	
+	Points.push_back(p);
+
+	p.Index = iterLimit;
+	p.SetColor(0xffffff);
+	Points.push_back(p);
+	
+	clrMapDataSky.InitColorMap(256, Points, 150);
+	colorMapSky = clrMapDataSky.GetColorMap();  
 }
 
 static MovingValue xAngle(0.055,0.001,-5,5,0.055,0.0);
@@ -64,22 +98,53 @@ static void drawGLScene()
     float h = glutGet( GLUT_WINDOW_HEIGHT );
     gluPerspective(60, w / h, 0.1, 300);
 
-    glMatrixMode( GL_MODELVIEW );
+    glMatrixMode( GL_MODELVIEW );    
+    
+    //=============================================
+    
+    glLoadIdentity();
+	
+	static float time = 0.0;
+	time += 0.003;
+	
+	glUseProgramObjectARB(shaderSky);
+	setUniform1f(shaderSky, "time", time);				
+	
+	glTexImage1D(GL_TEXTURE_1D, 0, GL_RGBA, 256, 0, GL_BGRA, GL_UNSIGNED_BYTE, colorMapSky);
+
+	
+	float skySize=10.0;
+	
+	glTranslatef(0,0,-5);
+	
+	glBegin(GL_QUADS);
+	glTexCoord2f(0, 0);
+	glVertex2f(-skySize, -skySize);
+	glTexCoord2f(1, 0);
+	glVertex2f(skySize, -skySize);
+	glTexCoord2f(1, 1);
+	glVertex2f(skySize, skySize);
+	glTexCoord2f(0, 1);
+	glVertex2f(-skySize, skySize);
+	glEnd();
+    
+	//=============================================
+	    
     glLoadIdentity();
     
-    glTranslatef(0,0,-2);
+    glTranslatef(0,0,-2.7);
     glRotatef(xAngle.step(),1,0,0);
     glRotatef(yAngle.step(),0,1,0);
 	glRotatef(zAngle.step(),0,0,1);
 
     
-    
-    
+    glUseProgramObjectARB(shaderProgram);
 	setUniform2f(shaderProgram, "c", koefReD.step(), koefImD.step());
 	setUniform1f(shaderProgram, "bound", movingHighBound.step());
 
 	glTexImage1D(GL_TEXTURE_1D, 0, GL_RGBA, iterLimit, 0, GL_BGRA, GL_UNSIGNED_BYTE, colorMap);
 	
+		
 	glBegin(GL_QUADS);
 	glTexCoord2f(0, 0);
 	glVertex2f(-1, -1);
@@ -90,7 +155,9 @@ static void drawGLScene()
 	glTexCoord2f(0, 1);
 	glVertex2f(-1, 1);
 	glEnd();
-
+	
+	//=============================================
+	
 	glutSwapBuffers();
 	
 	clrMapData.ColorMapStep();
@@ -140,6 +207,13 @@ int main(int argc, char **argv)
 
 	setUniform1i(shaderProgram, "iterLimit", iterLimit);
 	setUniform1f(shaderProgram, "checkDistanceSquare", checkDistanceSquare);
+	
+	
+	if(!(shaderSky = setupShader("sky.glsl"))) 
+	{	
+		return -1;
+	}
+			
 	
 	glutMainLoop();
 	return 0;
