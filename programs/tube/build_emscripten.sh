0df16b25eaadb9711b2f79a3498194a936df8b37
Vladimir Silaev <vovaprog@yandex.ru>
2017-10-20 10:11:52 +0300

    tube web assembly

--- programs/tube/build_emscripten.sh
+++ programs/tube/build_emscripten.sh
@@ -7,6 +7,16 @@ EMCC=~/tools/emscripten/emscripten/1.37.22/em++
 mkdir ./external
 cp --recursive /usr/include/glm ./external
 
-$EMCC -std=c++11 -O3 -Wall -I. -I$MYLIB -I./external ./tube.cpp ./LookAt.cpp $MYLIB/ColorMap.cpp -o ./tube.html -s LEGACY_GL_EMULATION=1 -lglut -lGL -lGLU --shell-file ./template.html 
+# Generate html with js file.
+OUTDIR=./build_emscripten/html
+rm --recursive $OUTDIR
+mkdir --parents $OUTDIR
+$EMCC -std=c++11 -O3 -Wall -I. -I$MYLIB -I./external ./tube.cpp ./LookAt.cpp $MYLIB/ColorMap.cpp -o $OUTDIR/tube.html -s LEGACY_GL_EMULATION=1 -lglut -lGL -lGLU --shell-file ./template.html
+
+# Generate html and compile code to web assembly.
+OUTDIR=./build_emscripten/webassembly
+rm --recursive $OUTDIR
+mkdir --parents $OUTDIR
+$EMCC -std=c++11 -O3 -Wall -I. -I$MYLIB -I./external ./tube.cpp ./LookAt.cpp $MYLIB/ColorMap.cpp -o $OUTDIR/tube.html -s WASM=1 -s LEGACY_GL_EMULATION=1 -lglut -lGL -lGLU --shell-file ./template.html
 
 rm --recursive ./external
--- programs/tube/build_linux.sh
+++ programs/tube/build_linux.sh
@@ -2,4 +2,8 @@
 
 MYLIB=../../mylib
 
-g++ -std=c++11 -O3 -march=native -Wall -I. -I$MYLIB ./tube.cpp $MYLIB/ColorMap.cpp -o ./tube -lglut -lGL -lGLU
+OUTDIR=./build
+rm --recursive $OUTDIR
+mkdir --parents $OUTDIR
+
+g++ -std=c++11 -O3 -march=native -Wall -I. -I$MYLIB ./tube.cpp $MYLIB/ColorMap.cpp -o $OUTDIR/tube -lglut -lGL -lGLU
